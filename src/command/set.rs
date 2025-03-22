8a6ef20edc57f9d60dc5ce34392523fc398df722
meannzen <senmeann@gmail.com>
2025-03-22 09:49:52 +0700

    Expire

--- src/command/set.rs
+++ src/command/set.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bytes::Bytes;
 
 use crate::{parse::Parse, store::Db, Connection, Frame};
@@ -6,13 +8,15 @@ use crate::{parse::Parse, store::Db, Connection, Frame};
 pub struct Set {
     key: String,
     value: Bytes,
+    expire: Option<Duration>,
 }
 
 impl Set {
-    pub fn new(key: impl ToString, value: Bytes) -> Self {
+    pub fn new(key: impl ToString, value: Bytes, expire: Option<Duration>) -> Self {
         Set {
             key: key.to_string(),
             value,
+            expire,
         }
     }
 
@@ -23,11 +27,26 @@ impl Set {
     pub fn parse_frame(parse: &mut Parse) -> crate::Result<Set> {
         let key = parse.next_string()?;
         let value = parse.next_bytes()?;
-        Ok(Set { key, value })
+        let mut expire = None;
+        match parse.next_string() {
+            Ok(s) if s.to_uppercase() == "EX" => {
+                let secs = parse.next_int()?;
+                expire = Some(Duration::from_secs(secs));
+            }
+            Ok(s) if s.to_uppercase() == "PX" => {
+                let mili = parse.next_int()?;
+                expire = Some(Duration::from_millis(mili));
+            }
+
+            Ok(_) => return Err("currently `SET` only supports the expiration option".into()),
+            Err(crate::parse::ParseError::EndOfStream) => {}
+            Err(err) => return Err(err.into()),
+        }
+        Ok(Set { key, value, expire })
     }
 
     pub async fn apply(self, db: &Db, dst: &mut Connection) -> crate::Result<()> {
-        db.set(self.key, self.value);
+        db.set(self.key, self.value, self.expire);
         let response = Frame::Simple("OK".to_string());
         dst.write_frame(&response).await?;
         Ok(())
--- src/store.rs
+++ src/store.rs
@@ -1,9 +1,13 @@
 use std::{
-    collections::HashMap,
+    collections::{BTreeSet, HashMap},
     sync::{Arc, Mutex},
 };
 
 use bytes::Bytes;
+use tokio::{
+    sync::Notify,
+    time::{self, Duration, Instant},
+};
 
 #[derive(Debug)]
 pub struct Store {
@@ -17,37 +21,110 @@ pub struct Db {
 #[derive(Debug)]
 struct Shared {
     state: Mutex<State>,
+    background_task: Notify,
 }
 
 #[derive(Debug)]
 struct State {
     entries: HashMap<String, Entry>,
+    expirations: BTreeSet<(Instant, String)>,
+    shutdown: bool,
 }
 
 #[derive(Debug)]
 struct Entry {
     data: Bytes,
+    expires_at: Option<Instant>,
 }
+impl Shared {
+    fn pure_expire_keys(&self) -> Option<Instant> {
+        let mut state = self.state.lock().unwrap();
+        if state.shutdown {
+            return None;
+        }
+        let state = &mut *state;
+        let now = Instant::now();
+        while let Some(&(when, ref key)) = state.expirations.iter().next() {
+            if when > now {
+                return Some(when);
+            }
 
+            state.entries.remove(key);
+            state.expirations.remove(&(when, key.clone()));
+        }
+
+        None
+    }
+    pub fn is_shutdown(&self) -> bool {
+        self.state.lock().unwrap().shutdown
+    }
+}
 impl Db {
     pub fn new() -> Self {
-        Db {
-            shared: Arc::new(Shared {
-                state: Mutex::new(State {
-                    entries: HashMap::new(),
-                }),
+        let shared = Arc::new(Shared {
+            state: Mutex::new(State {
+                entries: HashMap::new(),
+                expirations: BTreeSet::new(),
+                shutdown: false,
             }),
-        }
+
+            background_task: Notify::new(),
+        });
+        tokio::spawn(pure_expire_task(shared.clone()));
+
+        Db { shared }
     }
     pub fn get(&self, key: &str) -> Option<Bytes> {
         let state = self.shared.state.lock().unwrap();
         state.entries.get(key).map(|entry| entry.data.clone())
     }
 
-    pub fn set(&self, key: String, value: Bytes) {
+    pub fn set(&self, key: String, value: Bytes, expire: Option<Duration>) {
         let mut state = self.shared.state.lock().unwrap();
-        state.entries.insert(key.clone(), Entry { data: value });
+        let mut notify = false;
+        let expires_at = expire.map(|duration| {
+            let when = Instant::now() + duration;
+            notify = state
+                .next_expiration()
+                .map(|expiration| expiration > when)
+                .unwrap_or(true);
+            when
+        });
+
+        let prev = state.entries.insert(
+            key.clone(),
+            Entry {
+                data: value,
+                expires_at,
+            },
+        );
+        if let Some(prev) = prev {
+            if let Some(when) = prev.expires_at {
+                state.expirations.remove(&(when, key.clone()));
+            }
+        }
+
+        if let Some(when) = expires_at {
+            state.expirations.insert((when, key.clone()));
+        }
         drop(state);
+        if notify {
+            self.shared.background_task.notify_one();
+        }
+    }
+
+    fn shutdown_purk_task(&self) {
+        let mut state = self.shared.state.lock().unwrap();
+        state.shutdown = true;
+
+        drop(state);
+        self.shared.background_task.notify_one();
+    }
+}
+
+impl Drop for Store {
+    fn drop(&mut self) {
+        self.db.shutdown_purk_task();
     }
 }
 impl Default for Db {
@@ -64,5 +141,27 @@ impl Store {
 impl Default for Store {
     fn default() -> Self {
         Store::new()
+    }
+}
+
+impl State {
+    pub fn next_expiration(&self) -> Option<Instant> {
+        self.expirations
+            .iter()
+            .next()
+            .map(|expiration| expiration.0)
+    }
+}
+
+async fn pure_expire_task(shared: Arc<Shared>) {
+    while !shared.is_shutdown() {
+        if let Some(when) = shared.pure_expire_keys() {
+            tokio::select! {
+                _= time::sleep_until(when)=> {},
+                _= shared.background_task.notified() => {}
+            }
+        } else {
+            shared.background_task.notified().await;
+        }
     }
 }
