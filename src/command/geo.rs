a1e0d999ea36ce6984bb91aed2e775318ac8a08a
meannzen <senmeann@gmail.com>
2025-11-18 15:28:23 +0700

    Store a location

--- src/command/geo.rs
+++ src/command/geo.rs
@@ -1,3 +1,5 @@
+use bytes::Bytes;
+
 use crate::{
     frame::Frame,
     geometry::{validate_geo_coordinates, Coordinates},
@@ -10,7 +12,7 @@ use crate::{
 pub struct GeoAdd {
     key: String,
     coordinate: Coordinates,
-    member: String,
+    member: Bytes,
 }
 
 impl GeoAdd {
@@ -18,7 +20,7 @@ impl GeoAdd {
         let key = parse.next_string()?;
         let longitude: f64 = parse.next_string()?.parse()?;
         let latitude: f64 = parse.next_string()?.parse()?;
-        let member = parse.next_string()?;
+        let member = parse.next_bytes()?;
 
         Ok(GeoAdd {
             key,
@@ -30,13 +32,14 @@ impl GeoAdd {
         })
     }
 
-    pub async fn apply(self, _db: &Db, conn: &mut Connection) -> crate::Result<()> {
-        dbg!(self.key, self.member);
+    pub async fn apply(self, db: &Db, conn: &mut Connection) -> crate::Result<()> {
         let frame =
             match validate_geo_coordinates(self.coordinate.longitude, self.coordinate.latitude) {
                 Ok(_) => Frame::Integer(1),
                 Err(s) => Frame::Error(s.to_string()),
             };
+
+        db.zadd(self.key, self.member, 0.0);
 
         conn.write_frame(&frame).await?;
         Ok(())
